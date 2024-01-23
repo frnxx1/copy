28541a7f8c174d304d02f9af958f36b794dfee2b
awym.dev <awym.dev01@gmail.com>
2024-01-24 00:01:32 +0300

    start component sidebar

---
+++ src/widgets/sidebar/SideBar.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import styles from './SideBar.module.css';
+
+
+function SideBar() {
+  return (
+    <div className={styles.sidebar}>SideBar</div>
+  )
+}
+
+export default SideBar

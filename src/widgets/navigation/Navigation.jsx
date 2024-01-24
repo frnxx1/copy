b6af03aa3e1004c4dd5429111e18d6defef774d4
awym.dev <awym.dev01@gmail.com>
2024-01-25 02:07:58 +0300

    create new component categorybtn

---
+++ src/widgets/navigation/Navigation.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import styles from './Navigation.module.css';
+
+
+function Navigation() {
+  return (
+    <div className={styles.navigate}>
+
+        <div className={styles.navigateContainer}>
+
+            <div className={styles.navigateLogo}>
+
+            </div>
+
+            <div className={styles.searhContainer}>
+                
+            </div>
+
+            <div className={styles.kategory}>
+                
+            </div>
+
+        </div>
+
+    </div>
+  )
+}
+
+export default Navigation
